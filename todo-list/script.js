426a0ab3c2a24d0cb69cf92d0c94407b44ef5031
JENIFAR <jui@Juis-MacBook-Pro.local>
2025-08-28 21:19:31 +0600

    add

--- todo-list/script.js
+++ todo-list/script.js
@@ -5,15 +5,34 @@ function addtask(){
     tasklist.appendChild(newtask)
     newtask.textContent=document.getElementById('inputtask').value
     document.getElementById('inputtask').value=''
+
+    deletetask(newtask);
+}
+function deletetask(newtask){
+    let deletebtn=document.createElement('button')
     deletebtn.textContent='Delete';
     newtask.appendChild(deletebtn)
-    deletetask();
-}
-function deletetask(){
-    // let deletebtn=document.createElement('button')
-    // deletebtn.textContent='Delete';
-    // newtask.appendChild(deletebtn)
     deletebtn.onclick=function(){
         newtask.remove();
     }
 }
+{/* <script >
+    function addtask() {
+            let newtask = document.createElement('li');
+            let tasklist = document.getElementById('list');
+            let deletebtn = document.createElement('button')
+            tasklist.appendChild(newtask)
+            newtask.textContent = document.getElementById('inputtask').value
+            document.getElementById('inputtask').value = ''
+           
+            deletetask(newtask);
+        }
+        function deletetask(newtask) {
+            let deletebtn=document.createElement('button')
+            deletebtn.textContent='Delete';
+            newtask.appendChild(deletebtn)
+            deletebtn.onclick = function () {
+                newtask.remove();
+            }
+        }
+    </script> */}
